a462e90665b8246c43cd8869d9cc090b10531702
NOTEB <felipexbenevides@gmail.com>
2017-04-12 09:10:01 -0300

    Revert "Revert "AST""
    This reverts commit 2d4e752d34dc2aa0291bc116a3625299fa856a8c.

--- src/minipascal/AnaliseSintatica/Scanner.java
+++ src/minipascal/AnaliseSintatica/Scanner.java
@@ -281,8 +281,9 @@ public final class Scanner {
         //pos.finish = sourceFile.getCurrentLine();
         tok = new Token(kind, currentSpelling.toString());
         //if (debug)
-          System.out.print(tok.spelling + " ");
+        System.out.print(tok.spelling + " ");
         //return tok;
+        Data.tokenCount++;
         return tok;
     }
 
